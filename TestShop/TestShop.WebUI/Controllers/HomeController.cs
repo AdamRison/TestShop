f3c5546bc0b469d025bb2f0c8fce176e2e2e2c0d
Adam Rison <adamrison@gmail.com>
2020-01-07 19:40:55 -0500

    Added Product List!

--- TestShop/TestShop.WebUI/Controllers/HomeController.cs
+++ TestShop/TestShop.WebUI/Controllers/HomeController.cs
@@ -3,14 +3,40 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using TestShop.Core.Contracts;
+using TestShop.Core.Models;
 
 namespace TestShop.WebUI.Controllers
 {
     public class HomeController : Controller
     {
+        IRepository<Product> productRepository;
+        IRepository<ProductCategory> productCategoryRepository;
+
+        public HomeController(IRepository<Product> productContext, IRepository<ProductCategory> productCategoryContext)
+        {
+            productRepository = productContext;
+            productCategoryRepository = productCategoryContext;
+        }
+
         public ActionResult Index()
         {
-            return View();
+            List<Product> products = productRepository.Collection().ToList();
+            return View(products);
+        }
+
+        public ActionResult Details(string Id)
+        {
+            Product product = productRepository.Find(Id);
+
+            if(product == null)
+            {
+                return HttpNotFound();
+            }
+            else
+            {
+                return View(product);
+            }
         }
 
         public ActionResult About()
